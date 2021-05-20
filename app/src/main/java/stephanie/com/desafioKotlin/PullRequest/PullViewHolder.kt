1596725f7cd2e00a96030f8a0e6f7e8aa9a2e4b7
Pinheiro <stephanie.v.pinheiro@accenture.com>
2021-05-20 17:29:58 -0300

    Otimiza o binding ao usar apply

--- app/src/main/java/stephanie/com/desafioKotlin/PullRequest/PullViewHolder.kt
+++ app/src/main/java/stephanie/com/desafioKotlin/PullRequest/PullViewHolder.kt
@@ -5,12 +5,16 @@ import com.squareup.picasso.Picasso
 import stephanie.com.desafioKotlin.databinding.ItemPullRequestBinding
 import stephanie.com.desafioKotlin.modelo.PullRequest
 
-class PullViewHolder(val pullBinding: ItemPullRequestBinding): RecyclerView.ViewHolder(pullBinding.root) {
+class PullViewHolder(val pullBinding: ItemPullRequestBinding) :
+    RecyclerView.ViewHolder(pullBinding.root) {
     fun binding(pullRequest: PullRequest) {
-        pullBinding.pullRequestDate.text = pullRequest.criacaoPull
-        pullBinding.pullRequestDescricao.text = pullRequest.corpoPull
-        pullBinding.pullRequestName.text = pullRequest.user.nomePull
-        pullBinding.pullRequestTitle.text = pullRequest.tituloPull
+        pullBinding.apply {
+            pullRequestDate.text = pullRequest.criacaoPull
+            pullRequestDescricao.text = pullRequest.corpoPull
+            pullRequestName.text = pullRequest.user.nomePull
+            pullRequestTitle.text = pullRequest.tituloPull
+
+        }
         Picasso.get().load(pullRequest.user.fotoPull).into(pullBinding.fotoUsuarioPullRequest)
 
     }
