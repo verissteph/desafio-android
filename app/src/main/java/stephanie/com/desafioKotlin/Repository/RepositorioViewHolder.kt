706b4f5f993f76d0b4c89d28a15de273e4535305
Pinheiro <stephanie.v.pinheiro@accenture.com>
2021-05-20 17:20:45 -0300

    Separa RepositorioViewHolder do adapter

--- app/src/main/java/stephanie/com/desafioKotlin/Repository/RepositorioViewHolder.kt
+++ app/src/main/java/stephanie/com/desafioKotlin/Repository/RepositorioViewHolder.kt
@@ -1,4 +1,22 @@
 package stephanie.com.desafioKotlin.Repository
 
-class RepositoryViewHolder {
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import stephanie.com.desafioKotlin.databinding.ItemRepoBinding
+import stephanie.com.desafioKotlin.modelo.Repositorio
+
+class RepositorioViewHolder(val repoBinding: ItemRepoBinding) :
+    RecyclerView.ViewHolder(repoBinding.root) {
+
+    fun binding(repositorio: Repositorio) {
+        repoBinding.apply {
+            nomeRepositorio.text = repositorio.nomeRepo
+            descricaoRepositorio.text = repositorio.descricaoRepo
+            qdeForksRepositorio.text = repositorio.forksCountRepo
+            qdeStarsRepositorio.text = repositorio.stargazersCountRepo
+            usernameRepositorio.text = repositorio.owner.login
+            fullnameRepositorio.text = repositorio.fullNameRepo
+        }
+        Picasso.get().load(repositorio.owner.avatarUrl).into(repoBinding.fotoRepositorio);
+    }
 }
